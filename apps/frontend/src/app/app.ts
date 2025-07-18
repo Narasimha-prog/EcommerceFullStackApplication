198ca98f767bc361f6a6b741156ea0518c8d8d67
LN REDDY <lnreddyreddy405@gmail.com>
2025-07-18 20:49:16 +0530

    adding springboot application...

--- apps/frontend/src/app/app.ts
+++ apps/frontend/src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterModule } from '@angular/router';
 import { FaConfig, FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { fontAwesomeIcons } from './shared/font-awsome-icons';
 import { Navbar } from './layout/navbar/navbar';
+import { Footer } from "./layout/footer/footer";
 
 
 @Component({
   standalone: true,
-  imports: [RouterModule, FontAwesomeModule, Navbar],
+  imports: [RouterModule, FontAwesomeModule, Navbar, Footer],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
--- apps/frontend/src/app/layout/footer/footer.ts
+++ apps/frontend/src/app/layout/footer/footer.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule],
+  imports: [CommonModule,FontAwesomeModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
