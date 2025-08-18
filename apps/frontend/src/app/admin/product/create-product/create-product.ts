b44acbd4f4a5bf8feaad33d53392e8ff90e4f434
Lakshmi Narasimha Reddy <lnreddyreddy405@gmail.com>
2025-08-18 22:19:26 +0530

    Adding CreateProductComponenet dependent injection..!

--- apps/frontend/src/app/admin/product/create-product/create-product.ts
+++ apps/frontend/src/app/admin/product/create-product/create-product.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { AdminProductService } from '../../admin-product';
+import { Toast } from '../../../shared/model/toast/toast';
+import { Router } from '@angular/router';
+import { ProductPicture } from '../../model/product.model';
 
 @Component({
   selector: 'app-create-product',
@@ -7,4 +12,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './create-product.html',
   styleUrl: './create-product.scss',
 })
-export class CreateProductComponent {}
+export class CreateProductComponent {
+  formBuilder=inject(FormBuilder);
+  productService=inject(AdminProductService);
+  toastService=inject(Toast);
+  router=inject(Router);
+
+  public productPictures=new Array<ProductPicture>();
+
+name=new FormControl<string>('',{nonNullable: true});
+  
+}
