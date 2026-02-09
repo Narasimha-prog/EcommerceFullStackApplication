2ab7d5987069068d48adbfbd43521c57e0ff6720
Lakshmi Narasimha Reddy <lnreddyreddy405@gmail.com>
2026-02-09 13:16:37 +0530

    adding new kinde oauth redirect url

--- apps/frontend/src/environments/environment.production.ts
+++ apps/frontend/src/environments/environment.production.ts
@@ -3,8 +3,8 @@ export const environment = {
   apiUrl: "https://ecommercefullstackapplication.onrender.com/api",
   kinde: {
     authority: "https://lnreddy.kinde.com",
-    redirectUrl: "https://ecommercefullstackapplication-frontend.onrender.com",
-    postLogoutRedirectUri: "https://ecommercefullstackapplication-frontend.onrender.com",
+    redirectUrl: "https://angularfrontendecommerceapplication.onrender.com",
+    postLogoutRedirectUri: "https://angularfrontendecommerceapplication.onrender.com",
     clientId: "dcbd946d3ecf439c9030ea77a7b99caa",
     audience: "https://ecommercefullstackapplication.onrender.com/api"
   },
